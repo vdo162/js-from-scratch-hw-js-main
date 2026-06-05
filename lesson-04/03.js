c23ec262cbd2232afd1c74d30093908d23d8e865
Дмитрий Варенцов <vdo162@gmail.com>
2026-06-06 02:33:41 +0300

    lesson-4/4

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,4 +17,13 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(array1, array2) {
+    const newArr = []
+    for (let i = 0; i < array1.length; i++) {
+        const item = array1[i]
+        if (includesElement(array2, item)) {
+            newArr.push(item)
+        }
+    }
+    return newArr
+}
--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,10 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let newStr = ''
+    for (let i = 0; i < str.length; i++) {
+        newStr += str[i] + str[i]
+    }
+    return newStr
+}
