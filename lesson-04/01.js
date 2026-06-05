342a495b08bc6dfee1b5f10e26007f64b18fd694
Дмитрий Варенцов <vaitishnik162@gmail.com>
2026-06-06 02:07:59 +0300

    lesson-4/1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,13 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+    let isElement = false
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            isElement = true
+            break
+        }
+    }
+    return isElement
+}
